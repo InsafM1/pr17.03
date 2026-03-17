c403baba99297d3c36c0866c40684e454ec28aaa
InsafM1 <muhamadiarovinsaf0@gmail.com>
2026-03-17 13:26:02 +0300

    Construc, destruc and method

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -5,10 +5,35 @@ namespace topit
   template <class T>
   struct Vector
   {
+    Vector();
+    Vector(const Vector< T > v&) = delete;
+    ~Vector();
+    Vector< T >& operator= (const Vector< T >& v) = delete;
+
+    bool empty() const noexcept;
+
   private:
     T* data_;
-    size_t size_; capacity_;
-  };
+    size_t size_, capacity_;
+  }; 
+}
 
+template <class T>
+topit::Vector< T >::Vector():
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
+{}
+
+template <class T>
+topit::Vector< T >::~Vector()
+{
+  delete [] data_;
+}
+
+template <class T>
+bool topit::Vector< T >::empty()
+{
+  return data_; 
 }
 #endif
